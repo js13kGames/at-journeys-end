c5d2e2ad84db874e4e1318453add3cdbec562ad7
Kevin Beam <kwbeam@gmail.com>
2016-06-04 15:54:04 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function mvtswp(rootElement: HTMLElement) {
+    rootElement.innerText = "Welcome to the Minimum Viable TypeScript Webpack!";
+}
+
+let root = document.getElementById("app");
+mvtswp(root);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "./dist/bundle.js"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
+  },
+
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: "ts-loader" }
+    ],
+
+    preLoaders: [
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
