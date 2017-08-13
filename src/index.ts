3de90327a0b38e3b5c2778354e63ccfa478f728c
Kevin Beam <kwbeam@gmail.com>
2017-08-13 11:51:49 -0600

    Upgrade typescript related stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function game(rootElement: HTMLElement) {
-    rootElement.innerText = "Welcome to the absolute very best JS13k Game!";
+    rootElement.innerText = "Welcome to the best JS13k Game!";
 }
 
 let root = document.getElementById("app");
