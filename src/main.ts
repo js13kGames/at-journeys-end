9f72c58789f5aa6b5fb501c467395ef13975d13d
Kevin Beam <kwbeam@gmail.com>
2017-08-27 21:03:00 -0600

    Fix spaces -> tabs and make tabs 8 characters.

--- src/main.ts
+++ src/main.ts
@@ -9,153 +9,153 @@ import { AudioState, initSound, toggleSound, wind, knock } from './sound';
 //	- views an area of diameter CD.
 
 interface Point {
-    x: number
-    y: number
+	x: number
+	y: number
 }
-const Point = (x: number, y: number)=>({ x: x, y: y })
+const Point = (x: number, y: number) => ({ x: x, y: y })
 
 interface Size {
-    w: number
-    h: number
+	w: number
+	h: number
 }
-const Size = (w: number, h: number)=>({ w: w, h: h })
+const Size = (w: number, h: number) => ({ w: w, h: h })
 
 interface Tree {
-    center: Point
-    radius: number
+	center: Point
+	radius: number
 }
-const Tree = (center: Point, radius: number)=>({ center: center, radius: radius })
+const Tree = (center: Point, radius: number) => ({ center: center, radius: radius })
 
 // map world coordinates onto canvas
 interface Transform {
-    point: (wp: Point)=>Point
-    distance: (d: number)=>number
+	point: (wp: Point) => Point
+	distance: (d: number) => number
 }
 
 function getTransform(player: Point, wViewRadius: number, angle: number, canvasSize: Size): Transform {
 
-    // precalculate canvas center, canvas radius, and scale
-    const cx = canvasSize.w / 2
-    const cy = canvasSize.h / 2
-    const cr = Math.sqrt(cx * cx + cy * cy)
-    const scale = cr / wViewRadius
+	// precalculate canvas center, canvas radius, and scale
+	const cx = canvasSize.w / 2
+	const cy = canvasSize.h / 2
+	const cr = Math.sqrt(cx * cx + cy * cy)
+	const scale = cr / wViewRadius
 
-    return {
-	point: (p: Point)=>{
-	    const dx = p.x - player.x
-	    const dy = p.y - player.y
-	    const a = Math.atan2(dy, dx) - angle
-	    const d = Math.sqrt(dx * dx + dy * dy) * scale
-	    return Point(cx + d * Math.cos(a), cy + d * Math.sin(a))
-	},
-	distance: (d: number)=>d * scale
-    }
+	return {
+		point: (p: Point) => {
+			const dx = p.x - player.x
+			const dy = p.y - player.y
+			const a = Math.atan2(dy, dx) - angle
+			const d = Math.sqrt(dx * dx + dy * dy) * scale
+			return Point(cx + d * Math.cos(a), cy + d * Math.sin(a))
+		},
+		distance: (d: number) => d * scale
+	}
 }
 
 function main() {
-    const body = document.body.style
-    body.margin = "0px"
-    body.overflow = "hidden"
+	const body = document.body.style
+	body.margin = "0px"
+	body.overflow = "hidden"
 
-    const canvas = <HTMLCanvasElement>document.createElement("canvas");
-    document.body.appendChild(canvas);
-    const c = canvas.getContext('2d')
+	const canvas = <HTMLCanvasElement>document.createElement("canvas");
+	document.body.appendChild(canvas);
+	const c = canvas.getContext('2d')
 
-    // canvas dimensions
-    let canvasSize: Size
-    let canvasCenter: Point
+	// canvas dimensions
+	let canvasSize: Size
+	let canvasCenter: Point
 
-    // view dimensions
-    let worldViewRadius = 50
+	// view dimensions
+	let worldViewRadius = 50
 
-    let t = 0;
+	let t = 0;
 
-    let audioState = initSound();
-    wind(audioState);
+	let audioState = initSound();
+	wind(audioState);
 
-    // player position and rotation
-    const upAngle = 3 * Math.PI / 2
-    let direction = upAngle
-    let playerXY = Point(100, 100)
+	// player position and rotation
+	const upAngle = 3 * Math.PI / 2
+	let direction = upAngle
+	let playerXY = Point(100, 100)
 
-    function resize() {
-	setTimeout(()=>{
-	    canvasSize = Size(window.innerWidth, window.innerHeight)
-	    canvas.style.width = canvasSize.w + "px"
-	    canvas.style.height = canvasSize.h + "px"
-	    canvas.width = canvasSize.w
-	    canvas.height = canvasSize.h
-	    canvasCenter = Point(Math.round(canvasSize.w / 2), Math.round(canvasSize.h / 2))
-	    draw()
-	}, 10)
-    }
-
-    function action(key: Number) {
-	switch (key) {
-	case 65: direction -= 0.1; break;	// A left
-	case 68: direction += 0.1; break;	// D right
-	case 87: playerXY = move(playerXY, direction, 1); break;	// W up
-	case 83: playerXY = move(playerXY, direction + Math.PI, 1); break;	// S down
-	case 73: worldViewRadius--; break;	// I zoom in
-	case 75: worldViewRadius++; break;	// I // zoom out
-        case 78: knock(audioState); break;      // n 'knock'
-        case 81: toggleSound(audioState); break; // toggle the sound on/off
-	default: console.log(key)
+	function resize() {
+		setTimeout(() => {
+			canvasSize = Size(window.innerWidth, window.innerHeight)
+			canvas.style.width = canvasSize.w + "px"
+			canvas.style.height = canvasSize.h + "px"
+			canvas.width = canvasSize.w
+			canvas.height = canvasSize.h
+			canvasCenter = Point(Math.round(canvasSize.w / 2), Math.round(canvasSize.h / 2))
+			draw()
+		}, 10)
 	}
-    }
 
-    function move(from: Point, direction: number, distance: number): Point {
-	return Point(from.x + Math.cos(direction) * distance, from.y + Math.sin(direction) * distance)
-    }
+	function action(key: Number) {
+		switch (key) {
+			case 65: direction -= 0.1; break;	// A left
+			case 68: direction += 0.1; break;	// D right
+			case 87: playerXY = move(playerXY, direction, 1); break;	// W up
+			case 83: playerXY = move(playerXY, direction + Math.PI, 1); break;	// S down
+			case 73: worldViewRadius--; break;	// I zoom in
+			case 75: worldViewRadius++; break;	// I // zoom out
+			case 78: knock(audioState); break;      // n 'knock'
+			case 81: toggleSound(audioState); break; // toggle the sound on/off
+			default: console.log(key)
+		}
+	}
 
-    window.addEventListener("resize", resize)
-    window.addEventListener("keydown", e=>action(e.keyCode))
-    resize()
+	function move(from: Point, direction: number, distance: number): Point {
+		return Point(from.x + Math.cos(direction) * distance, from.y + Math.sin(direction) * distance)
+	}
 
-    const trees = [Tree(Point(110, 120), 0.5)]
+	window.addEventListener("resize", resize)
+	window.addEventListener("keydown", e => action(e.keyCode))
+	resize()
 
-    function draw() {
-        t += 0.1;
+	const trees = [Tree(Point(110, 120), 0.5)]
 
-        // clear the canvas
-        c.fillStyle = "rgba(0,0,0,1)"
-        c.fillRect(0, 0, canvasSize.w, canvasSize.h);
+	function draw() {
+		t += 0.1;
 
-        // draw the light
-        const lr = 800;
-        const g = c.createRadialGradient(canvasCenter.x, canvasCenter.y, 0, canvasCenter.x, canvasCenter.y, lr);
+		// clear the canvas
+		c.fillStyle = "rgba(0,0,0,1)"
+		c.fillRect(0, 0, canvasSize.w, canvasSize.h);
 
-        const baseIntensity = 1, flickerAmount = 0.1;
-        const intensity = baseIntensity + flickerAmount * (0.578 - (Math.sin(t) + Math.sin(2.2 * t + 5.52) + Math.sin(2.9 * t + 0.93) + Math.sin(4.6 * t + 8.94))) / 4;
+		// draw the light
+		const lr = 800;
+		const g = c.createRadialGradient(canvasCenter.x, canvasCenter.y, 0, canvasCenter.x, canvasCenter.y, lr);
 
-        const steps = 32; // number of gradient steps
-        const lightScale = 15; // controls how quickly the light falls off
-        for (var i = 1; i < steps + 1; i++) {
-            let x = lightScale * Math.pow(i / steps, 2) + 1;
-            let alpha = intensity / (x * x);
-            g.addColorStop((x - 1) / lightScale, `rgba(255,255,255,${alpha})`);
-        }
+		const baseIntensity = 1, flickerAmount = 0.1;
+		const intensity = baseIntensity + flickerAmount * (0.578 - (Math.sin(t) + Math.sin(2.2 * t + 5.52) + Math.sin(2.9 * t + 0.93) + Math.sin(4.6 * t + 8.94))) / 4;
 
-        c.fillStyle = g
-        c.fillRect(canvasCenter.x - lr, canvasCenter.y - lr, lr*2, lr*2)
+		const steps = 32; // number of gradient steps
+		const lightScale = 15; // controls how quickly the light falls off
+		for (var i = 1; i < steps + 1; i++) {
+			let x = lightScale * Math.pow(i / steps, 2) + 1;
+			let alpha = intensity / (x * x);
+			g.addColorStop((x - 1) / lightScale, `rgba(255,255,255,${alpha})`);
+		}
 
-	// draw the center
-	c.strokeStyle = "blue"
-	c.strokeRect(canvasCenter.x - 5, canvasCenter.y - 5, 10, 10)
+		c.fillStyle = g
+		c.fillRect(canvasCenter.x - lr, canvasCenter.y - lr, lr * 2, lr * 2)
 
-	const transform = getTransform(playerXY, worldViewRadius, direction - upAngle, canvasSize)
-	trees.forEach(tree=>{
-	    const vCenter = transform.point(tree.center)
-	    const vRadius = transform.distance(tree.radius)
-	    c.beginPath()
-	    c.fillStyle = "green"
-	    c.arc(vCenter.x, vCenter.y, vRadius, 0, Math.PI * 2)
-	    c.fill()
-	})
+		// draw the center
+		c.strokeStyle = "blue"
+		c.strokeRect(canvasCenter.x - 5, canvasCenter.y - 5, 10, 10)
 
-	// refactor the game loop
-	window.requestAnimationFrame(draw)
-    }
+		const transform = getTransform(playerXY, worldViewRadius, direction - upAngle, canvasSize)
+		trees.forEach(tree => {
+			const vCenter = transform.point(tree.center)
+			const vRadius = transform.distance(tree.radius)
+			c.beginPath()
+			c.fillStyle = "green"
+			c.arc(vCenter.x, vCenter.y, vRadius, 0, Math.PI * 2)
+			c.fill()
+		})
+
+		// refactor the game loop
+		window.requestAnimationFrame(draw)
+	}
 }
 
 main()
--- src/sound.ts
+++ src/sound.ts
@@ -1,56 +1,56 @@
 export interface AudioState {
-        context: AudioContext;
-        totalGain: GainNode;
+	context: AudioContext;
+	totalGain: GainNode;
 }
 
 export function initSound(): AudioState {
-        let context = new AudioContext();
-        var totalGain = context.createGain();
-        totalGain.connect(context.destination);
+	let context = new AudioContext();
+	var totalGain = context.createGain();
+	totalGain.connect(context.destination);
 
-        return { context, totalGain }
+	return { context, totalGain }
 }
 
 export function toggleSound(audio: AudioState) {
-        audio.totalGain.gain.value = audio.totalGain.gain.value ? 0 : 1;
+	audio.totalGain.gain.value = audio.totalGain.gain.value ? 0 : 1;
 }
 
 // The basic random noise generator was lifted from this helpful post:
 // https://noisehack.com/generate-noise-web-audio-api/
 export function wind(audio: AudioState) {
-        const bufferSize = 4096;
+	const bufferSize = 4096;
 
-        let gain = audio.context.createGain();
-        gain.connect(audio.totalGain);
-        gain.gain.setValueAtTime(0.08, audio.context.currentTime);
+	let gain = audio.context.createGain();
+	gain.connect(audio.totalGain);
+	gain.gain.setValueAtTime(0.08, audio.context.currentTime);
 
-        let lastOut = 0.0;
-        let node = audio.context.createScriptProcessor(bufferSize, 1, 1);
-        node.onaudioprocess = function(e) {
-                let output = e.outputBuffer.getChannelData(0);
-                for (let i = 0; i < bufferSize; i++) {
-                        let white = Math.random() * 2 - 1;
-                        output[i] = (lastOut + (0.02 * white)) / 1.02;
-                        lastOut = output[i];
-                        output[i] *= 3.5; // (roughly) compensate for gain
-                }
-        }
+	let lastOut = 0.0;
+	let node = audio.context.createScriptProcessor(bufferSize, 1, 1);
+	node.onaudioprocess = function(e) {
+		let output = e.outputBuffer.getChannelData(0);
+		for (let i = 0; i < bufferSize; i++) {
+			let white = Math.random() * 2 - 1;
+			output[i] = (lastOut + (0.02 * white)) / 1.02;
+			lastOut = output[i];
+			output[i] *= 3.5; // (roughly) compensate for gain
+		}
+	}
 
-        node.connect(gain);
+	node.connect(gain);
 }
 
 export function knock(audio: AudioState) {
-        var gain = audio.context.createGain();
-        gain.connect(audio.totalGain);
-        let now = audio.context.currentTime
-        gain.gain.setValueAtTime(1, now);
-        gain.gain.exponentialRampToValueAtTime(0.01, now + 0.1);
+	var gain = audio.context.createGain();
+	gain.connect(audio.totalGain);
+	let now = audio.context.currentTime
+	gain.gain.setValueAtTime(1, now);
+	gain.gain.exponentialRampToValueAtTime(0.01, now + 0.1);
 
-        let oscillator = audio.context.createOscillator();
-        oscillator.type = "triangle";
-        oscillator.frequency.value = 80;
-        oscillator.connect(gain);
+	let oscillator = audio.context.createOscillator();
+	oscillator.type = "triangle";
+	oscillator.frequency.value = 80;
+	oscillator.connect(gain);
 
-        oscillator.start(now);
-        oscillator.stop(now + 0.1);
+	oscillator.start(now);
+	oscillator.stop(now + 0.1);
 }
