561457545cd05d35169c75735e7ee889e747c148
Todd Conley <ToddConley@gmail.com>
2017-09-03 13:46:03 -0600

    Reworked projection and primitives, included latest map from Corson

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,167 +1,156 @@
-import { distance, Config, Point, Size } from './geometry';
+import { distance, Config, XYZ, XY, LWH, LW, RA, LWToRA, RAToXYZ, XYZPlusXYZ, XYMinusXY, XYDistance } from './geometry';
 
-export interface Tree {
-	center: Point
-	radius: number
+export interface Primitive {
+	draw(c: Config): void
 }
 
-export function Tree(center: Point, radius: number) {
-	return { center: center, radius: radius }
-}
+// cuboid
+export function Box(xyz: XYZ, lwh: LWH, a: number): Primitive {
+	return {
+		draw: (c: Config)=>{
+			if (XYDistance(XYMinusXY(xyz, c.cameraXYZ)) > c.worldViewRadius) return
 
-export function drawTree(tree: Tree, config: Config) {
-	const h = distance(tree.center.x - config.playerXY.x, tree.center.y - config.playerXY.y) // hypotenuse
-	if (h > config.worldViewRadius) return
+			const topZ = Math.min(xyz.z + lwh.h, c.cameraXYZ.z * .99)
+			const bps = corners(xyz, lwh, a)				// bottom corners
+			const tps = bps.map(bp=>XYZ(bp.x, bp.y, topZ))	// top corners
+			const xys = c.transform.xyzs([...bps, ...tps])
 
-	const angleBetweenCenterAndTangent = Math.asin(tree.radius / h)
-	const angleToTree = Math.atan2(tree.center.y - config.playerXY.y, tree.center.x - config.playerXY.x)
-	const a1 = angleToTree - angleBetweenCenterAndTangent
-	const a2 = angleToTree + angleBetweenCenterAndTangent
-
-	const p1 = config.transform.point(Point(config.playerXY.x + Math.cos(a1) * h,
-		config.playerXY.y + Math.sin(a1) * h))
-	const p2 = config.transform.point(Point(config.playerXY.x + Math.cos(a1) * config.worldViewRadius,
-		config.playerXY.y + Math.sin(a1) * config.worldViewRadius))
-	const p3 = config.transform.point(Point(config.playerXY.x + Math.cos(a2) * config.worldViewRadius,
-			config.playerXY.y + Math.sin(a2) * config.worldViewRadius))
-	// p4 isn't needed because of how arcTo() works
-	const pw = config.transform.point(config.playerXY) // player in WC
-
-	config.context2d.moveTo(p1.x, p1.y)
-	config.context2d.lineTo(p2.x, p2.y)
-	config.context2d.lineTo(p3.x, p3.y)
-	config.context2d.arcTo(pw.x, pw.y, p1.x, p1.y, config.transform.distance(tree.radius)) // tricky
-}
-
-export interface Block {
-	center: Point
-	size: Size
-	angle: number
-	height?: number
-	z?: number
-}
-
-export function Block(center: Point, size: Size, angle: number, height?: number, z?: number) {
-	return { center: center, size: size, angle: angle, height: height, z: z }
-}
-
-export function drawBlock(block: Block, config: Config) {
-	if (distance(block.center.x - config.playerXY.x, block.center.y - config.playerXY.y) > config.worldViewRadius) return
-
-	const h = distance(block.size.w, block.size.h) / 2
-	const a = Math.atan2(block.size.h, block.size.w)
-	const a1 = a + block.angle
-	const a2 = Math.PI - a + block.angle
-	const a3 = Math.PI + a + block.angle
-	const a4 = -a + block.angle
-
-	// determine corners in WC
-	const corners = [
-		Point(block.center.x + Math.cos(a1) * h, block.center.y + Math.sin(a1) * h),
-		Point(block.center.x + Math.cos(a2) * h, block.center.y + Math.sin(a2) * h),
-		Point(block.center.x + Math.cos(a3) * h, block.center.y + Math.sin(a3) * h),
-		Point(block.center.x + Math.cos(a4) * h, block.center.y + Math.sin(a4) * h)
-	]
-
-	// transform corners
-	const cps = corners.map(p=>config.transform.point(p))
-
-	// cast a ray through each corner
-	function castRay(corner: Point) {
-		const a = Math.atan2(corner.y - config.playerXY.y, corner.x - config.playerXY.x)
-		const cp = config.transform.point(corner)
-		const sl = shadowLength()
-		const rp = config.transform.point(Point(config.playerXY.x + Math.cos(a) * sl, config.playerXY.y + Math.sin(a) * sl))
-		return { a: a, cp: cp, rp: rp }
-
-		function shadowLength() {
-			const cameraHeight = 30
-			if (block.height < cameraHeight) {
-				const d = distance(corner.x - config.playerXY.x, corner.y - config.playerXY.y)
-				const sl = d + d * block.height / (cameraHeight - block.height)
-				return sl
+			// draw the four vertical sides
+			for (let i = 0; i < 4; i++) {
+				c.lib.beginPath()
+				const j = (i + 1) % 4
+				moveTo(xys[i], c)
+				linesTo([xys[i+4], xys[j+4], xys[j], xys[i]], c)
+				c.lib.fill()
 			}
-			return config.worldViewRadius
+
+			// draw the top
+			c.lib.beginPath()
+			moveTo(xys[4], c)
+			linesTo([xys[5], xys[6], xys[7], xys[4]], c)
+			c.lib.fill()
 		}
 	}
+}
 
-	// start with left side
-	const ps = corners.map(corner=>castRay(corner))
-	for (let i = 0; i < 4; i++) {
-		for (let j = i; j < 4; j++) {
-			if (ps[i].a - ps[j].a > Math.PI) ps[j].a += Math.PI * 2
-			if (ps[j].a - ps[i].a > Math.PI) ps[i].a += Math.PI * 2
+// cylinder
+export function Can(xyz: XYZ, r: number, h: number): Primitive {
+	return {
+		draw: (c: Config)=>{
+			
+			// find distance
+			const dxy = XYMinusXY(xyz, c.cameraXYZ)
+			const d = XYDistance(dxy)
+			if (d > c.worldViewRadius) return
+
+			// find left and right point of tangency at bottom of cylinder
+			const angleToCenter = Math.atan2(dxy.y, dxy.x)
+			const angleBetweenCenterAndTangent = Math.asin(r / d)
+			const a1 = angleToCenter - angleBetweenCenterAndTangent
+			const a2 = angleToCenter + angleBetweenCenterAndTangent
+			const bp1 = XYZ(c.cameraXYZ.x + Math.cos(a1) * d, c.cameraXYZ.y + Math.sin(a1) * d, xyz.z)
+			const bp2 = XYZ(c.cameraXYZ.x + Math.cos(a2) * d, c.cameraXYZ.y + Math.sin(a2) * d, xyz.z)
+			const bxys = c.transform.xyzs([bp1, bp2, xyz, XYZ(c.cameraXYZ.x, c.cameraXYZ.y, 0)])
+
+			// find left and right point of tangency at top of cylinder
+			const topZ = Math.min(xyz.z + h, c.cameraXYZ.z * .99)
+			const tp1 = XYZ(bp1.x, bp1.y, topZ)
+			const tp2 = XYZ(bp2.x, bp2.y, topZ)
+			const txys = c.transform.xyzs([tp1, tp2, XYZ(xyz.x, xyz.y, topZ)])
+
+			// draw
+			c.lib.beginPath()
+			moveTo(bxys[0], c)
+			linesTo([txys[0], txys[1]], c)
+			c.lib.arcTo(bxys[3].x, bxys[3].y, txys[0].x, txys[0].y, XYDistance(XYMinusXY(bxys[0], bxys[2])))
+			c.lib.fill()
+			c.lib.beginPath()
+			c.lib.arc(txys[2].x, txys[2].y, XYDistance(XYMinusXY(txys[0], txys[2])), 0, 2 * Math.PI)
+			c.lib.fill()
 		}
 	}
+}
 
-	let first = 0
-	let min = 10
-	ps.forEach((p, i)=>{
-		if (p.a < min) {
-			first = i
-			min = p.a
-		}
-	})
-	for (let i = 0; i < first; i++) ps.push(ps.shift())
-
-	const center = config.transform.point(config.playerXY)
-
-	config.context2d.beginPath()
-	config.context2d.moveTo(ps[0].cp.x, ps[0].cp.y)
-	config.context2d.lineTo(ps[0].rp.x, ps[0].rp.y)
-	let last = ps[0]
-	for (let i = 1; i < 4; i++) {
-		if (ps[i].a >= last.a) {
-			last = ps[i]
-			config.context2d.lineTo(ps[i].rp.x, ps[i].rp.y)
+// rectangle on the ground, z is ignored
+export function Rug(xyz: XYZ, lw: LW, a: number): Primitive {
+	return {
+		draw: (c: Config)=>{
+			if (XYDistance(XYMinusXY(xyz, c.playerXY)) > c.worldViewRadius + Math.max(lw.l, lw.w)) return
+			const wps = corners(xyz, lw, a)
+			const cps = c.transform.xyzs(wps)
+			c.lib.beginPath()
+			moveTo(cps[0], c)
+			linesTo([cps[1], cps[2], cps[3], cps[0]], c)
+			c.lib.fill()
 		}
 	}
-	config.context2d.lineTo(last.cp.x, last.cp.y)
-	config.context2d.fillStyle = "black"
-	config.context2d.fill()
-
-	// draw block
-	config.context2d.beginPath()
-	config.context2d.moveTo(cps[0].x, cps[0].y)
-	config.context2d.lineTo(cps[1].x, cps[1].y)
-	config.context2d.lineTo(cps[2].x, cps[2].y)
-	config.context2d.lineTo(cps[3].x, cps[3].y)
-	config.context2d.lineTo(cps[0].x, cps[0].y)
-	config.context2d.fillStyle = "black"
-	config.context2d.fill()
 }
 
-export interface Plate {
-	center: Point
-	size: Size
-	angle: number
+export function corners(xyz: XYZ, lw: LW, a: number): XYZ[] {
+	const ra = LWToRA(lw)
+	const ras = [RA(ra.r, ra.a+a), RA(ra.r, Math.PI-ra.a+a), RA(ra.r, Math.PI+ra.a+a), RA(ra.r, -ra.a+a)]
+	const result = ras.map(ra=>XYZPlusXYZ(xyz, RAToXYZ(ra)))
+	return result
 }
 
-export function Plate(center: Point, size: Size, angle: number): Plate {
-	return { center: center, size: size, angle: angle }
+export function moveTo(xy: XY, c: Config) {
+	c.lib.moveTo(xy.x, xy.y)
 }
 
-export function drawPlate(plate: Plate, config: Config) {
-	const hyp = distance(plate.size.w, plate.size.h) / 2
-	const angle = Math.atan2(plate.size.h, plate.size.w)
-
-
-	const a1 = angle + plate.angle
-	const a2 = -angle + plate.angle
-	const a3 = a1 + Math.PI
-	const a4 = a2 + Math.PI
-
-	const p1 = config.transform.point(Point(plate.center.x + Math.cos(a1) * hyp, plate.center.y + Math.sin(a1) * hyp))
-	const p2 = config.transform.point(Point(plate.center.x + Math.cos(a2) * hyp, plate.center.y + Math.sin(a2) * hyp))
-	const p3 = config.transform.point(Point(plate.center.x + Math.cos(a3) * hyp, plate.center.y + Math.sin(a3) * hyp))
-	const p4 = config.transform.point(Point(plate.center.x + Math.cos(a4) * hyp, plate.center.y + Math.sin(a4) * hyp))
-
-	config.context2d.beginPath()
-	config.context2d.moveTo(p1.x, p1.y)
-	config.context2d.lineTo(p2.x, p2.y)
-	config.context2d.lineTo(p3.x, p3.y)
-	config.context2d.lineTo(p4.x, p4.y)
-	config.context2d.lineTo(p1.x, p1.y)
-	config.context2d.fillStyle = "black"
-	config.context2d.fill()
+export function lineTo(xy: XY, c: Config) {
+	c.lib.lineTo(xy.x, xy.y)
 }
+
+export function linesTo(xys: XY[], c: Config) {
+	xys.forEach(xy=>lineTo(xy, c))
+}
+
+export function Rain(c: Config): XYZ {
+	const r = c.worldViewRadius / 1
+	const x = c.playerXY.x + (Math.random() * 2 - 1) * r
+	const y = c.playerXY.y + (Math.random() * 2 - 1) * r
+	const z = Math.random() * (c.cameraXYZ.z - 1)
+	return XYZ(x, y, z)
+}
+
+export function drawRain(p: XYZ, c: Config) {
+	const r = c.worldViewRadius / 1
+
+	// start a new drop when the drop has hit
+	if (p.z < 1) {
+		p.x = c.playerXY.x + (Math.random() * 2 - 1) * r
+		p.y = c.playerXY.y + (Math.random() * 2 - 1) * r
+		p.z = c.cameraXYZ.z * .99 - Math.random()
+	}
+	const d = distance(c.playerXY.x - p.x, c.playerXY.y - p.y, p.z)
+	const light = Math.min(Math.max(Math.round((1 - d / r) * 6), 0), 6)
+	const p1 = c.transform.xyz(p)
+	p.z -= 2
+	const p2 = c.transform.xyz(p)
+	p.z -= 2
+
+	c.lib.beginPath()
+	c.lib.moveTo(p1.x, p1.y)
+	c.lib.lineTo(p2.x, p2.y)
+	c.lib.strokeStyle = "#" + light + light + light
+	c.lib.stroke()
+}
+
+	/*
+	//const trees = generateTrees(1000, 6)
+	
+	// divide the map into zones and put one tree in each zone
+	function generateTrees(mapSize: number, zoneSize: number): Tree[] {
+		const trees = [] as Tree[]
+		for (let zx = 0; zx * zoneSize < mapSize; zx++) {
+			for (let zy = 0; zy * zoneSize < mapSize; zy++) {
+				const r = Math.random() / 2 + 0.3
+				const tx = zx * zoneSize + Math.random() * (zoneSize - r * 2) + r
+				const ty = zy * zoneSize + Math.random() * (zoneSize - r * 2) + r
+				trees.push(Tree(Point(tx, ty), r))
+			}
+		}
+		return trees
+	}
+	*/
+
