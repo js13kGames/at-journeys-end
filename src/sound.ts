a1fd511d37d14be86e15334318d17cc64c0126a8
Kevin Beam <kwbeam@gmail.com>
2017-09-04 21:45:34 -0600

    WIP: play multiple notes at a time and only specify note, duration.

--- src/sound.ts
+++ src/sound.ts
@@ -1,8 +1,13 @@
 const GAME_VOLUME = 0.8;
 
-const organR = new Float32Array([0, 1.0, 0.5, 0.25, 0.125, 0.06, 0.03, 0.015, 0.0075, 0.00375]);
-const organI = new Float32Array(organR.length);
-let organTable = (context: AudioContext) => context.createPeriodicWave(organR, organI);
+const openOrganR = new Float32Array([0, 1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 0.0078125, 0.00390625]);
+//const openOrganR = new Float32Array([0, 1.0, 0.95, 0.9025, 0.857375, 0.8145]);
+const openOrganI = new Float32Array(openOrganR.length);
+const closedOrganR = new Float32Array([0, 1.0, 0.0, 0.6, 0.0, 0.4, 0.0, 0.25, 0.0, 0.16]);
+const closedOrganI = new Float32Array(closedOrganR.length);
+let openOrganTable = (context: AudioContext) => context.createPeriodicWave(openOrganR, openOrganI);
+let closedOrganTable = (context: AudioContext) => context.createPeriodicWave(closedOrganR, closedOrganI);
+let organTable = closedOrganTable;
 
 export interface AudioState {
 	context: AudioContext;
@@ -62,21 +67,27 @@ export function wind(audio: AudioState) {
 }
 
 // TODO: Refactor so we don't care about start / end (see playOrgan).
-function playOrganNote(audio: AudioState, frequency: number, start: number, end: number) {
-	var gain = audio.context.createGain();
+function playOrganNote(audio: AudioState, spec: AudioSpec) {
+	let [frequency, start, end] = spec;
+
+	let gain = audio.context.createGain();
 	gain.connect(audio.totalGain);
+
+	var filter = audio.context.createBiquadFilter();
+	filter.connect(gain);
+	filter.frequency.value = 500;
+	filter.type = 'allpass';
 
 	let o = audio.context.createOscillator();
 	o.setPeriodicWave(audio.organWave);
 	o.frequency.value = frequency;
-	o.connect(gain);
+	o.connect(filter);
 
-	// TODO: How do we get smoother approach / falloff?
 	let absStart: number = audio.context.currentTime + start;
 	let absEnd: number = audio.context.currentTime + end;
-	gain.gain.setValueAtTime(0.01, absStart)
-	gain.gain.exponentialRampToValueAtTime(1, absStart + 0.01);
-	gain.gain.exponentialRampToValueAtTime(0.1, absEnd + 0.025);
+	gain.gain.setValueAtTime(0, absStart)
+	gain.gain.linearRampToValueAtTime(1, absStart + 0.01);
+	gain.gain.linearRampToValueAtTime(0.001, absEnd + 0.02);
 	o.start(absStart);
 	o.stop(absEnd);
 }
@@ -86,30 +97,58 @@ interface Map<T> {
 	[key: string]: T;
 };
 
-// TODO: Expand this to include a full range of notes
-const NOTE_FREQS: Map<number> = {
-	"C3": 130.81,
-	"D3": 146.83,
-	"E3": 164.81,
-	"F3": 174.61,
-	"G3": 196.00,
-	"A3": 220.00,
-	"B3": 246.94
-};
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const octaves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let NOTE_FREQS: Map<number> = {};
+octaves.forEach(o => {
+	for (let n of NOTES) {
+		let od = o - 4;
+		let hs = od * 12 + (NOTES.indexOf(n) - NOTES.indexOf('A'));
+		NOTE_FREQS[n + o] = 440.0 * Math.pow(1.059463094359, hs);
+	}
+});
 
-// TODO: Fix the types
-function playScore(audio: AudioState, score: any) {
-	score.forEach((n: any) => playOrganNote(audio, NOTE_FREQS[n[0]], n[1], n[2]));
+type AudioSpec = [number, number, number];
+type NoteSpec = [string, number];
+
+function audioSpec(bpm: number, t: number, n: any, d: number): AudioSpec[] {
+	return n.split(',').map((e: string) => [NOTE_FREQS[e], t, t + 1 / (d * bpm)]);
+}
+
+function playScore(audio: AudioState, score: NoteSpec[], bpm: number) {
+	let t = 0;
+	let spec: AudioSpec[] = [];
+	score.forEach((v, i, a) => {
+		let [n, d] = v;
+		let s: AudioSpec[] = audioSpec(bpm, t, n, d);
+		spec = spec.concat(s);
+		t += 1 / (d * bpm);
+	});
+	spec.forEach(s => playOrganNote(audio, s));
 }
 
 export function playOrgan(audio: AudioState) {
-	// TODO: We shouldn't have to specify absolute times just to get a chord.
-	// Refactor this to be able to just play a note or chord, and a duration.
-	let score = [["D3", 0, 0.6], ["F3", 0, 0.6], ["A3", 0, 0.6],
-	             ["E3", 0.6, 0.9],
-	             ["F3", 0.9, 1.2],
-	             ["G3", 1.2, 1.5],
-	             ["D3", 1.5, 2.1], ["F3", 1.5, 2.1], ["A3", 1.5, 2.1],
-	             ["D3", 2.4, 3.6], ["F3", 2.4, 3.6], ["A3", 2.4, 3.6]];
-	playScore(audio, score);
+	let scales: NoteSpec[] = [['C3', 1], ['D3', 1], ['E3', 1], ['F3', 1], ['G3', 1], ['A3', 1], ['B3', 1], ['C4', 1],
+	['C3', 2], ['D3', 2], ['E3', 2], ['F3', 2], ['G3', 2], ['A3', 2], ['B3', 2], ['C4', 2],
+	['C3', 4], ['D3', 4], ['E3', 4], ['F3', 4], ['G3', 4], ['A3', 4], ['B3', 4], ['C4', 4],
+	['C3', 8], ['D3', 8], ['E3', 8], ['F3', 8], ['G3', 8], ['A3', 8], ['B3', 8], ['C4', 8],
+	['C4', 8], ['D4', 8], ['E4', 8], ['F4', 8], ['G4', 8], ['A4', 8], ['B4', 8], ['C5', 8],
+	['C4', 4], ['D4', 4], ['E4', 4], ['F4', 4], ['G4', 4], ['A4', 4], ['B4', 4], ['C5', 4],
+	['C4', 2], ['D4', 2], ['E4', 2], ['F4', 2], ['G4', 2], ['A4', 2], ['B4', 2], ['C5', 2],
+	['C4', 1], ['D4', 1], ['E4', 1], ['F4', 1], ['G4', 1], ['A4', 1], ['B4', 1], ['C5', 1]];
+
+	let hb: NoteSpec[] = [['G2', 4], ['G2', 8], ['A2', 4], ['G2', 4], ['C3', 4], ['B2', 3 / 2],
+	['G2', 4], ['G2', 8], ['A2', 4], ['G2', 4], ['D3', 4], ['C3', 3 / 2],
+	['G2', 4], ['G2', 8], ['G3', 4], ['E3', 4], ['C3', 4], ['B2', 4], ['A2', 3 / 2],
+	['F3', 4], ['F3', 8], ['E3', 4], ['C3', 4], ['D3', 4], ['C3', 3 / 2]];
+
+	let fugue: NoteSpec[] = [['D3,D2,D4', 2], ['A3,A2,A4', 2], ['F3,F2,F4', 2], ['D3,D2,D4', 2], ['C#3,C#2,C#4', 2],
+	['D3,D2,D4', 4], ['E3,E2,E4', 4], ['F3,F2,F4', 2], ['F3,F2,F4', 4], ['G3,G2,G4', 4],
+	['F3,F2,F4', 4], ['E3,E2,E4', 4], ['D3,D2,D4', 4], ['D0', 4],
+	['A3,A4', 1], ['A#3,A#4', 1],
+	['G3,G4', 1], ['A3,A4', 1],
+	['F3,F4', 1], ['G3,G4', 1],
+	['F1,F2,F3,F4', 2], ['E1,E2,E3,E4', 2], ['D1,D2,D3,D4', 1]];
+
+	playScore(audio, fugue, 1);
 }
